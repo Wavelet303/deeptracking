980ea7f415ed79369dc6b10ae87180a76e1c1754
MathGaron <mathieugaron91@gmail.com>
2017-04-12 11:39:20 -0400

    add scoring function

--- bin/evaluate.py
+++ bin/evaluate.py
@@ -2,14 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import os
 matplotlib.style.use('ggplot')
 
 if __name__ == '__main__':
-    path = "../train_test_nogit/eval_diff.csv"
+    path = "../train_test_nogit"
+    path_csv = os.path.join(path, "eval_diff.csv")
 
-    df = pd.read_csv(path)
+    fig, axes = plt.subplots(nrows=2, ncols=1)
+
+    df = pd.read_csv(path_csv)
     column_tags = df.columns.values
-    df.plot(x=np.arange(len(df)), y=column_tags[:3])
+    df.plot(ax=axes[0], x=np.arange(len(df)), y=column_tags[:3])
+    df.plot(ax=axes[1], x=np.arange(len(df)), y=column_tags[3:])
+    fig = plt.gcf()
+    fig.savefig(os.path.join(path, "plot.png"))
     plt.show()
-    df.plot(x=np.arange(len(df)), y=column_tags[3:])
-    plt.show()
+
+    # Compute scores data
+    info = pd.DataFrame()
+    info["mean"] = df.mean(axis=0)
+    info["std"] = df.std(axis=0)
+    info.to_csv(os.path.join(path, "score.csv"), index=True, encoding='utf-8')
